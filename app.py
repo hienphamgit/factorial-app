33fd98008532816f9d6728a54ceb48e120c1e09a
Hiên Phạm <126763241+hienphamgit@users.noreply.github.com>
2025-07-21 16:37:12 +0700

    factorial app v1

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from factorial import fact
+
+def main():
+    st.title("Factorial Calculator")
+    
+    number = st.number_input("Enter a number:", min_value=0, max_value=999, value=0, step=1)
+    
+    if st.button("Calculate Factorial"):
+        result = fact(number)
+        st.write(f"The factorial of {number} is {result}")
+if __name__ == "__main__":
+    main()
