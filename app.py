77eba370dc1ac842dbe85cec57ec573a6a4e3ed4
Hiên Phạm <hienphambmh@gmail.com>
2025-07-22 07:28:03 +0700

    abc

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import streamlit as st
 from factorial import fact
+import factorial as f
+import pandas as pd
 
 def main():
-    st.title("Factorial Calculator")
-    st.write("This app calculates the factorial of a given number.")    
+    st.title("Mô hình dự báo tỷ giá USD/VND")    
+    # chia màn hình thành 2 cột
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        from_date = st.date_input("Chọn ngày bắt đầu")
+        to_date = st.date_input("Chọn ngày kết thúc")
+        loading_data_button = st.button("Tải dữ liệu")
+    with col2:
+        if loading_data_button:
+            #tạo loading spinner khi tải dữ liệu
+            with st.spinner("Đang tải dữ liệu..."):
+                df = f.get_data(from_date, to_date)
+                if df is not None:
+                    # Hiển thị mô tả thống kê
+                    st.write("Mô tả thống kê:")
+                    st.write(df.describe())
     
-    number = st.number_input("Enter a number:", min_value=0, max_value=999, value=0, step=1)
+
     
-    if st.button("Calculate Factorial"):
-        result = fact(number)
-        st.write(f"The factorial of {number} is {result}")
+                
+
+
 if __name__ == "__main__":
     main()
--- factorial.py
+++ factorial.py
@@ -1,4 +1,31 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from arch import arch_model
+import warnings
+
+warnings.filterwarnings("ignore")
+
 def fact(n):
     if n == 0 or n == 1:
         return 1
     return n * fact(n - 1)
+
+# --- 1. Thu thập dữ liệu ---
+# Sử dụng yfinance để tải dữ liệu USD/VND (sử dụng mã cặp tiền VND=X)
+def get_data(from_date, to_date):
+    from_date_string = from_date.strftime('%Y-%m-%d')
+    to_date_string = to_date.strftime('%Y-%m-%d')
+    try:
+        data = yf.download("VND=X", start=from_date_string, end=to_date_string)
+    except Exception as e:
+        print(f"Lỗi khi tải dữ liệu: {e}")
+        return None
+    if data.empty:
+        print("Không có dữ liệu trong khoảng thời gian đã chọn.")
+        return None
+    data = data[['Close']].rename(columns={'Close': 'USD_VND'})
+    df = pd.DataFrame(data) # Gán MultiIndex vào cột
+    df.columns = df.columns.droplevel(1) # Loại bỏ cấp độ thứ 2 (VND=X)
+    return df
