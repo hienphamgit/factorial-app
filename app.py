d9cfac8a2fc16c9b5b2c6a57e9ecd659b7819dd7
hienphamgit <hienphambmh@gmail.com>
2026-02-12 09:32:51 +0700

    .

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def main():
     df['Còn lại cần nhập'] = df['Còn lại cần nhập'].apply(lambda x: 0 if x < 0 else x)
     # Sắp xếp theo tổng đã nhập từ cao đến thấp
     df_sorted = df.sort_values('Tổng đã nhập', ascending=True)
+
     # Tạo biểu đồ bar ngang, với mỗi thanh bar hiên thị 3 phần: Đã nhập (cũ), Số mới nhập, Còn lại cần nhập
     fig, ax = plt.subplots(figsize=(10, 8))
     bar_width = 0.25
@@ -47,9 +48,20 @@ def main():
     ax.set_ylabel('Tỉnh')
     ax.set_title('Tình hình nhập theo tỉnh')
     ax.legend()
-    st.pyplot(fig)
-    st.write(df_sorted)
 
+    
+    # Hiển thị 2 cột : cột bảng số liệu và cột biểu đồ
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.subheader("Bảng số liệu")
+        # Hiển thị bảng số liệu với các cột: Tỉnh, Số cần nhập, Số mới nhập, Tổng đã nhập, Tỷ lệ hoàn thành
+        df_sorted['Tỷ lệ hoàn thành'] = df_sorted['Tổng đã nhập'] / df_sorted['Số cần nhập'] * 100
+        df_data = df_sorted[['Tỉnh', 'Số cần nhập', 'Số mới nhập', 'Tổng đã nhập']]
+        st.dataframe(df_sorted[['Tỉnh', 'Số cần nhập',  'Số mới nhập', 'Tổng đã nhập']].reset_index(drop=True))
+    with col2:
+        st.subheader("Biểu đồ")
+        st.pyplot(fig)
+    
 
 
 if __name__ == "__main__":
