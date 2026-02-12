30c55db6c4ffa8a3c2d44e256afa72e23e1d909c
hienphamgit <hienphambmh@gmail.com>
2026-02-12 09:06:02 +0700

    .

--- app.py
+++ app.py
@@ -5,8 +5,26 @@ import numpy as np
 
 def main():
     st.title("Cập nhật tình hình nhập liệu trên nền tảng")    
+    # Nhập dữ liệu từ bảng
+    data = {
+        'Tỉnh': ['An Giang', 'Bắc Ninh', 'Cà Mau', 'Cao Bằng', 'Cần Thơ', 'Đà Nẵng', 
+                'Đắk Lắk', 'Điện Biên', 'Đồng Nai', 'Đồng Tháp', 'Gia Lai', 'Hà Nội',
+                'Hà Tĩnh', 'Hải Phòng', 'Hồ Chí Minh', 'Hưng Yên', 'Khánh Hòa', 'Kiên Giang',
+                'Lạng Sơn', 'Lào Cai', 'Lâm Đồng', 'Nghệ An', 'Ninh Bình', 'Phú Thọ',
+                'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sơn La', 'Tây Ninh', 'Thái Nguyên',
+                'Thanh Hóa', 'Huế', 'Tuyên Quang', 'Vĩnh Long'],
+        'Số cần nhập': [426, 378, 188, 473, 80, 430, 182, 66, 457, 222, 287, 2367, 121, 504,
+                        53, 154, 355, 47, 365, 38, 147, 291, 363, 261, 166, 1951, 125, 80, 
+                        173, 235, 120, 86, 120, 173],
+        'Số mới nhập': [12, 0, 0, 0, 12, 99, 0, 3, 39, 0, 0, 103, 1, 1, 38, 0, 0, 11, 0, 0,
+                        0, 3, 1, 0, 4, 0, 1, 0, 0, 2, 31, 15, 1, 0],
+        'Tổng đã nhập': [201, 99, 99, 93, 227, 721, 149, 61, 457, 84, 193, 1099, 7, 170, 410,
+                        135, 265, 381, 66, 164, 176, 104, 116, 88, 164, 1979, 141, 63, 427,
+                        53, 637, 286, 75, 192]
+    }
 
-    
+    df = pd.DataFrame(data)
+    st.write(df)
 
 if __name__ == "__main__":
     main()
