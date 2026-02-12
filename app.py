4c4a3c0e65b00648d023e7ca0152d05f6afb060f
hienphamgit <hienphambmh@gmail.com>
2026-02-12 10:44:45 +0700

    .

--- app.py
+++ app.py
@@ -65,28 +65,32 @@ def main():
 
     #Dọc dữ liệu từ file Data.xlsx tại sheet tinh va khuvuc
 
-    df_tinh = pd.read_excel('Data.xlsx', sheet_name='tinh')
-    df_khuvuc = pd.read_excel('Data.xlsx', sheet_name='khuvuc')
-
-    st.dataframe(df_tinh)
+    df_tinh = pd.read_excel('tinh.xlsx')
+    # Lấy dữ liệu nguồn tin về tội phạm bao gồm các cột Tỉnh, Số cần nhập NTTP, Số mới nhập  NTTP, Tổng đã nhập NTTP
+    data_tintoipham = df_tinh[['Tỉnh', 'Số cần nhập NTTP', 'Số mới nhập NTTP', 'Tổng đã nhập NTTP']].copy()
+    data_tintoipham = data_tintoipham.rename(columns={
+        'Số cần nhập NTTP': 'Số cần nhập',
+        'Số mới nhập NTTP': 'Số mới nhập',
+        'Tổng đã nhập NTTP': 'Tổng đã nhập'
+    })
 
     # Nhập dữ liệu từ bảng
-    data_tintoipham = {
-        'Tỉnh': ['An Giang', 'Bắc Ninh', 'Cà Mau', 'Cao Bằng', 'Cần Thơ', 'Đà Nẵng', 
-                'Đắk Lắk', 'Điện Biên', 'Đồng Nai', 'Đồng Tháp', 'Gia Lai', 'Hà Nội',
-                'Hà Tĩnh', 'Hải Phòng', 'Hồ Chí Minh', 'Hưng Yên', 'Khánh Hòa', 'Kiên Giang',
-                'Lạng Sơn', 'Lào Cai', 'Lâm Đồng', 'Nghệ An', 'Ninh Bình', 'Phú Thọ',
-                'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sơn La', 'Tây Ninh', 'Thái Nguyên',
-                'Thanh Hóa', 'Huế', 'Tuyên Quang', 'Vĩnh Long'],
-        'Số cần nhập': [426, 378, 188, 473, 80, 430, 182, 66, 457, 222, 287, 2367, 121, 504,
-                        53, 154, 355, 47, 365, 38, 147, 291, 363, 261, 166, 1951, 125, 80, 
-                        173, 235, 120, 86, 120, 173],
-        'Số mới nhập': [12, 0, 0, 0, 12, 99, 0, 3, 39, 0, 0, 103, 1, 1, 38, 0, 0, 11, 0, 0,
-                        0, 3, 1, 0, 4, 0, 1, 0, 0, 2, 31, 15, 1, 0],
-        'Tổng đã nhập': [201, 99, 99, 93, 227, 721, 149, 61, 457, 84, 193, 1099, 7, 170, 410,
-                        135, 265, 381, 66, 164, 176, 104, 116, 88, 164, 1979, 141, 63, 427,
-                        53, 637, 286, 75, 192]
-    }
+    # data_tintoipham = {
+    #     'Tỉnh': ['An Giang', 'Bắc Ninh', 'Cà Mau', 'Cao Bằng', 'Cần Thơ', 'Đà Nẵng', 
+    #             'Đắk Lắk', 'Điện Biên', 'Đồng Nai', 'Đồng Tháp', 'Gia Lai', 'Hà Nội',
+    #             'Hà Tĩnh', 'Hải Phòng', 'Hồ Chí Minh', 'Hưng Yên', 'Khánh Hòa', 'Kiên Giang',
+    #             'Lạng Sơn', 'Lào Cai', 'Lâm Đồng', 'Nghệ An', 'Ninh Bình', 'Phú Thọ',
+    #             'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sơn La', 'Tây Ninh', 'Thái Nguyên',
+    #             'Thanh Hóa', 'Huế', 'Tuyên Quang', 'Vĩnh Long'],
+    #     'Số cần nhập': [426, 378, 188, 473, 80, 430, 182, 66, 457, 222, 287, 2367, 121, 504,
+    #                     53, 154, 355, 47, 365, 38, 147, 291, 363, 261, 166, 1951, 125, 80, 
+    #                     173, 235, 120, 86, 120, 173],
+    #     'Số mới nhập': [12, 0, 0, 0, 12, 99, 0, 3, 39, 0, 0, 103, 1, 1, 38, 0, 0, 11, 0, 0,
+    #                     0, 3, 1, 0, 4, 0, 1, 0, 0, 2, 31, 15, 1, 0],
+    #     'Tổng đã nhập': [201, 99, 99, 93, 227, 721, 149, 61, 457, 84, 193, 1099, 7, 170, 410,
+    #                     135, 265, 381, 66, 164, 176, 104, 116, 88, 164, 1979, 141, 63, 427,
+    #                     53, 637, 286, 75, 192]
+    # }
 
     df = pd.DataFrame(data_tintoipham)
     df = format_data(df)
